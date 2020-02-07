e4c71853e16aae79ead8cd2d481e1a83d0c8a29f
Jarvis <jarvismiranda@gmail.com>
2020-02-07 14:50:35 -0500

    inicio

---
+++ PhoneGapProjects/App_MariloginsaTM/www/_instantUpdateSettings.js
@@ -0,0 +1,12 @@
+function _instantUpdateSettings() {
+return {
+	"baseLineGUID": "fce48f02b7f44fc88d2ba210aebbac9b",
+	"baseURL": "http://phonegapinstantupdate.s3.amazonaws.com/",
+	"displayMessageAfterUpdate": false,
+	"message": "",
+	"systemMessages": "Verbose",
+	"autoUpdate": true,
+	"displayFirstTimeLoadMessage": true,
+	"firstTimeRunMessage": "Please wait while the app is prepared for first-time use."
+};
+}
