930c027f59c07763a9df1d099bc992bce8089dc8
Jarvis <jarvismiranda@gmail.com>
2020-07-31 14:12:27 -0500

    11

--- PhoneGapProjects/App_TFN/www/_instantUpdateSettings.js
+++ PhoneGapProjects/App_TFN/www/_instantUpdateSettings.js
@@ -1,6 +1,6 @@
 function _instantUpdateSettings() {
 return {
-	"baseLineGUID": "3198e9854a0349a9b55ae245465c6193",
+	"baseLineGUID": "08e2c5dacd60481a98549f12044eb546",
 	"baseURL": "http://phonegapinstantupdate.s3.amazonaws.com/",
 	"displayMessageAfterUpdate": false,
 	"message": "",
