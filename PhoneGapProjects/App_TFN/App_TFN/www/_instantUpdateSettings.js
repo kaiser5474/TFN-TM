e2786815d2686472115ffddf951576bed45f9a8e
Jarvis <jarvismiranda@gmail.com>
2020-09-29 13:58:05 -0500

    111

---
+++ PhoneGapProjects/App_TFN/App_TFN/www/_instantUpdateSettings.js
@@ -0,0 +1,12 @@
+function _instantUpdateSettings() {
+return {
+	"baseLineGUID": "51b1654b204941c9bedee1018885ddb7",
+	"baseURL": "http://phonegapinstantupdate.s3.amazonaws.com/",
+	"displayMessageAfterUpdate": false,
+	"message": "",
+	"systemMessages": "Verbose",
+	"autoUpdate": true,
+	"displayFirstTimeLoadMessage": true,
+	"firstTimeRunMessage": "Please wait while the app is prepared for first-time use."
+};
+}
